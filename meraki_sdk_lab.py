6ec16dc4c57c93cece56095556309ff54f370b11
Marc Wilson <posguy99@gmail.com>
2022-07-12 18:18:06 -0700

    Initial commit

---
+++ meraki_sdk_lab.py
@@ -0,0 +1,42 @@
+import meraki 
+import json 
+
+# configuration parameters
+
+# Configuration parameters and credentials for MERAKI
+API_KEY = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+
+resp = meraki.DashboardAPI (api_key=API_KEY)
+
+# get the organizations
+orgs = resp.organizations.getOrganizations()
+
+params = {}
+print ("*"*20)
+print ("Organizations")
+for org in orgs:
+    #display the organizations
+    print("Organization Name: {}".format(org[ "name"]))
+    print ("Organization Id: {}".format(org["id"]))
+    print("="*5)
+
+    #remember the organization info flagged
+    if org ["name"] == "DevNet Sandbox" :
+        params ["organizationId"] = org["id" ]
+print ("*"*40)
+
+#get the organization's networks
+networks = resp.organizations.getOrganizationNetworks \
+    (params["organizationId"])
+#get the networks
+print ("Networks:")
+for network in networks:
+    print ("Network Name: {}".format (network["id"]))
+    print ("Network Name: {}". format (network[ "name"]))
+    print("="*5)
+
+    #remember a specific network
+    if network["name"] == "DevNet Sandbox ALWAYS ON" :
+        params["networkId"] = network["id"]
+print ("*"*40)
+
